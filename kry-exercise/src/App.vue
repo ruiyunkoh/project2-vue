07793e226f6bb56dc026dbb0545ee8a4f1b64d35
ruiyunkoh <ruiyun.k@gmail.com>
2022-01-16 14:08:41 +0000

    started vue and created navbar

---
+++ kry-exercise/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+   <div class="container-fluid">
+     <nav class="nav sticky-top">
+      <div id="exerciseLogo">
+        <img alt="" src="./assets/logo.png">
+      </div>
+      <a class="nav-link active mx-2" aria-current="page" >HOME</a>
+      <a class="nav-link mx-2">CREATE</a>  
+     </nav>
+   </div>
+ </div>
+
+</template>
+
+<script>
+export default {
+  name: 'App',
+  
+};
+</script>
+
+<style>
+#exerciseLogo > img{
+  height: 80px;
+  width: 60px;
+}
+</style>
