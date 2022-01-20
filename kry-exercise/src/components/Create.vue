27cca276c744b92dd42be297657d3f8d3bc6160b
ruiyunkoh <ruiyun.k@gmail.com>
2022-01-20 06:40:52 +0000

    created form for new exercise

--- kry-exercise/src/components/Create.vue
+++ kry-exercise/src/components/Create.vue
@@ -1,13 +1,132 @@
 <template>
-  <h1>Create exercises</h1>
+  <div class="create-holder row">
+    <h1>Share your routine!</h1>
+    <form class="row g-3 mt-3">
+      <div class="col-md-8">
+        <label class="form-label">Title:</label>
+        <input type="text" class="form-control" v-model="title">
+      </div>
+      <div class="col-md-4">
+        <label class="form-label">Your Name:</label>
+        <input type="text" class="form-control" v-model="poster">
+      </div>
+      <div class="col-12">
+        <label class="form-label">Exercise Image URL:</label>
+        <input type="text" class="form-control" v-model="image">
+      </div>
+      <div class="col-12">
+        <label class="form-label">Description:</label>
+        <input type="text" class="form-control" v-model="description" placeholder="Enter short description of your routine">
+      </div>
+      <div class="mb-3">
+        <label class="form-label">Routine:</label>
+        <textarea class="form-control" v-model="routine" rows="3" placeholder="Separate each move by an exclamation mark"></textarea>
+      </div>      
+      <div class="col-md-4">
+        <label class="form-label">Exercise Type:</label>
+        <select v-model="type" class="form-select">
+          <option selected value="HIIT">HIIT</option>
+          <option value="Cardio">Cardio</option>
+          <option value="Stretching">Stretching</option>
+          <option value="Yoga/Pilates">Yoga/Pilates</option>
+        </select>
+      </div>
+      <div class="col-md-4">
+        <label class="form-label">Target Area:</label>
+        <select v-model="target_area" class="form-select">
+          <option selected value="Whole Body">Whole Body</option>
+          <option value="Upper body">Upper body</option>
+          <option value="Legs">Legs</option>
+          <option value="Butt">Butt</option>
+          <option value="Abs">Abs</option>
+        </select>
+      </div>
+      <div class="col-md-2">
+        <label class="form-label">Duration (mins):</label>
+        <input type="text" class="form-control" v-model="duration">
+      </div>
+      <div class="col-md-2">
+        <label class="form-label">Calories burnt:</label>
+        <input type="text" class="form-control" v-model="calories_burnt">
+      </div>
+      <div class="me-2">
+        <label class="ms-1 me-2">Intensity:</label>
+        <input type="radio" name ="intensity" value="Beginner" v-model="intensity"> Beginner
+        <input type="radio" name ="intensity" value="Intermediate" v-model="intensity"> Intermediate
+        <input type="radio" name ="intensity" value="Advanced" v-model="intensity"> Advanced
+      </div>
+      <div class="tags-list ">
+        <label class="form-check-label ms-1 me-2">Tags:</label>
+        <div>
+        <input class="form-check-input ms-2" type="checkbox" value="No equipment required" v-model="tags"> No equipment required
+        <input class="form-check-input ms-2" type="checkbox" value="No jumping" v-model="tags"> No jumping
+        </div>
+      </div>
+      <button type="button" class="create-btn btn-outline-secondary col-1 my-3">Add</button>
+      
+    </form>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
+
+const API_URL = "https://kry-exercise.herokuapp.com";
+
 export default {
   name: 'Create',
-}
+  data: function () {
+    return {
+      poster: "",
+      title: "",
+      image: "",
+      duration: "",
+      description: "",
+      routine: "",
+      type: "",
+      intensity: "",
+      "target_area": "",
+      "calories_burnt": "",
+      tags: []
+    };
+  },
+  methods: {
+   processAdd: async function () {
+     let response = await axios.post(API_URL + "/new_exercise", {
+      poster: this.poster,
+      title: this.title,
+      image: this.image,
+      duration: this.duration,
+      description: this.description,
+      routine: this.routine,
+      type: this.type,
+      intensity: this.intensity,
+      "target_area": this.target_area,
+      "calories_burnt": this.calories_burnt,
+      tags: this.tags
+     });
+     console.log(response.data);
+    },
+ },
+};
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Shippori+Antique&display=swap");
+.create-holder>h1{
+  display: flex;
+  justify-content: center;
+  font-family: "Roboto Condensed", sans-serif;
+  margin-top: 10px;
+}
+.create-holder{
+  display: flex;
+  justify-content: center;
+  
+}
+form {
+  max-width: 800px !important;
+  
+}
 
 </style>
